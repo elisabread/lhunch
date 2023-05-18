7925a1d8219eb1a0fdd6c00d61dda6a4f2badb0b
Elisabeth Nyström <elisabethnystrom@elisabeths-mbp.home>
2023-05-18 16:55:16 +0200

    Cleaned up code.

---
+++ src/Enums/Weekdays.ts
@@ -0,0 +1,7 @@
+export enum weekdays {
+    "Måndag",
+    "Tisdag",
+    "Onsdag",
+    "Torsdag",
+    "Fredag",
+  }
---
+++ src/Types/MenuItem.ts
@@ -0,0 +1,6 @@
+import { weekdays } from "../Enums/Weekdays";
+
+export type menuItem = {
+    day: weekdays;
+    choices: string[];
+  };
